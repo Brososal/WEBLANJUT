57aa63ed93c6b0973c88cffb6a14817f35f9b68f
unknown <2311102441228@umkt.ac.id>
2025-06-28 12:18:10 +0800

    update settings.py for Render deployment

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,11 +1,16 @@
-import sys
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
 import os
 
-path = '/home/Brosoal/WEBLANJUT'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'WEBLANJUT.settings'
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
 application = get_wsgi_application()
